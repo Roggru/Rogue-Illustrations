38065443b2b16992d0be2b6d01470cb4a13cada7
Roggru <rogguillustration@gmail.com>
2025-12-27 06:21:06 -0600

    Page work

--- jsc/scripts.js
+++ jsc/scripts.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 const cordImageFilename = "Knight-Wander-3.png";
-const pages = ["portfolio.html", "end.html", "Divine-Beings/melthildkhyne.html"];
+const pages = ["portfolio.html", "end.html", "Divine-Beings/arabas.html", "Divine-Beings/melthildkhyne.html"];
 
 //Wanderer
 function setupRandomLink() {
